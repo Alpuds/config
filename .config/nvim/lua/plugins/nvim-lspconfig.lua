1aa5c73e102739f98404d3133cd13263c32be590
Alpuds <sduanuf@gmail.com>
2026-09-02 01:47:09 -0700

    removed deprecated diagnostic navigation
    goto_next and goto_prev are deprecated.
    Instead, use ]d and [d to jump to next and previous diagnostic
    receptively.
    Jumping to a diagnostic will automatically show a floating window with
    the message.
    
    Also removed custom keybinding for code action.
    Use default gra keybinding since it is consistent with other lsp
    keybindings.

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,11 +3,9 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition, {silent = true})
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {silent = true})
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {silent = true})
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {silent = true})
-vim.keymap.set("n", "<leader>lca", vim.lsp.buf.code_action, {silent = true})
 vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {silent = true})
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, {silent = true})
-vim.keymap.set("n", "<C-n>", vim.diagnostic.goto_next, {silent = true})
-vim.keymap.set("n", "<C-p>", vim.diagnostic.goto_prev, {silent = true})
+vim.diagnostic.config({ jump = { float = true }})
 
 -- Specify how the border looks like
 local border = {
